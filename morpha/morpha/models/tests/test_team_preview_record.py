6aa057999dbcc9e484b31e6e6a284134ceaa54fd
ubuntu <dnguyen0304@gmail.com>
2017-04-14 14:34:43 -0400

    ME-630: Fixed inconsistent naming convention.

--- morpha/morpha/models/tests/test_team_preview_record.py
+++ morpha/morpha/models/tests/test_team_preview_record.py
@@ -5,21 +5,21 @@ from nose.tools import assert_equal, raises
 from .. import TeamPreviewRecord
 
 
-class TestTeamPreviewRecordParser(object):
+class TestTeamPreviewRecord(object):
 
-    def test_parse(self):
+    def test_from_message(self):
         message = '|poke|p1|Foo, F|item'
         record = TeamPreviewRecord.from_message(message)
         assert_equal(record.player_id, 1)
         assert_equal(record.pokemon_name, 'Foo')
 
-    def test_parse_without_gender(self):
+    def test_from_message_without_gender(self):
         message = '|poke|p1|Foo|item'
         record = TeamPreviewRecord.from_message(message)
         assert_equal(record.player_id, 1)
         assert_equal(record.pokemon_name, 'Foo')
 
     @raises(ValueError)
-    def test_parse_incorrect_format(self):
+    def test_from_message_incorrect_format(self):
         message = '|foo'
         TeamPreviewRecord.from_message(message)
