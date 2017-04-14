1b607a4093924f2aa82479a2ce64fda04e05e583
ubuntu <dnguyen0304@gmail.com>
2017-04-14 12:01:01 -0400

    ME-630: Moved HtmlParser to the models.battle_log module.

--- morpha/morpha/main.py
+++ morpha/morpha/main.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import sys
 
-if sys.version_info < (3, 0):
-    import HTMLParser as html_parser
+def main():
+
+    pass
 
 
-class HtmlParser(html_parser.HTMLParser):
-
-    def __init__(self):
-        html_parser.HTMLParser.__init__(self)
-        self._is_battle_log = False
-        self.processed_data = ''
-
-    def handle_starttag(self, tag, attributes):
-        if tag == 'script':
-            for attribute in attributes:
-                if attribute[0] != 'class':
-                    continue
-                if attribute[1] == 'battle-log-data':
-                    self._is_battle_log = True
-
-    def handle_data(self, data):
-        if self._is_battle_log:
-            self._is_battle_log = False
-            self.processed_data = data
+if __name__ == '__main__':
+    main()
--- morpha/morpha/models/tests/test_battle_log.py
+++ morpha/morpha/models/tests/test_battle_log.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from nose.tools import assert_equal, assert_list_equal, assert_true
+from nose.tools import (assert_equal,
+                        assert_false,
+                        assert_list_equal,
+                        assert_true)
 
 from .. import IRecord
-from ..battle_log import Parser
+from ..battle_log import HtmlParser, Parser
 
 
 class MockRecord(IRecord):
@@ -14,6 +17,28 @@ class MockRecord(IRecord):
     @classmethod
     def from_message(cls, message):
         return cls()
+
+
+class TestHtmlParser(object):
+
+    def __init__(self):
+        self.html_parser = None
+        self.data = ''
+
+    def setup(self):
+        self.html_parser = HtmlParser()
+        self.data = '<script class="battle-log-data">foo</script>'
+
+    def test_interface(self):
+        assert_true(hasattr(self.html_parser, 'processed_data'))
+
+    def test_parse_battle_log_html(self):
+        self.html_parser.feed(data=self.data)
+        assert_equal(self.html_parser.processed_data, 'foo')
+
+    def test_parse_battle_log_flag(self):
+        self.html_parser.feed(data=self.data)
+        assert_false(self.html_parser._is_battle_log)
 
 
 class TestParser(object):
