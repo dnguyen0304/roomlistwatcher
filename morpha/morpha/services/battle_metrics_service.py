5d5d7951b1b21fac86aad30cf905aa138903e3ff
ubuntu <dnguyen0304@gmail.com>
2017-04-17 17:57:19 -0400

    ME-630: Changed to use _handle_log_records method.
    Changed summary DataFrame to have a MultiIndex.

--- morpha/morpha/services/battle_metrics_service.py
+++ morpha/morpha/services/battle_metrics_service.py
@@ -11,22 +11,28 @@ class BattleMetricsService(object):
 
     def __init__(self):
         self._battle = models.Battle()
+        self.summary = pd.DataFrame()
 
     def read_html(self, file_path):
         log = models.BattleLog.from_html(file_path=file_path)
-        for log_record in log.records:
-            self._battle.apply_log_record(log_record)
+        self._handle_log_records(log.records)
 
     def read_string(self, data):
         log = models.BattleLog.from_string(data=data)
-        for log_record in log.records:
-            self._battle.apply_log_record(log_record)
+        self._handle_log_records(log.records)
 
-    @property
-    def summary(self):
-        data = list()
+    def _handle_log_records(self, log_records):
+        for log_record in log_records:
+            self._battle.apply_log_record(log_record)
+            if self._battle.pokemon_are_loaded:
+                index = self._construct_index()
+                self.summary = pd.DataFrame(index=index)
+
+    def _construct_index(self):
+        tuples = list()
         for player in self._battle.get_all_players():
             pokemon_names = (pokemon.name for pokemon in player.pokemon)
-            data.extend(itertools.product([player.name], pokemon_names))
-        columns = ['player_name', 'pokemon_name']
-        return pd.DataFrame.from_records(data=data, columns=columns)
+            tuples.extend(itertools.product([player.name], pokemon_names))
+        names = ['player_name', 'pokemon_name']
+        index = pd.MultiIndex.from_tuples(tuples, names=names)
+        return index
--- morpha/morpha/services/tests/test_battle_metrics_service.py
+++ morpha/morpha/services/tests/test_battle_metrics_service.py
@@ -8,8 +8,11 @@ from .. import BattleMetricsService
 class TestBattleMetricsService(object):
 
     def test_summary_fields(self):
-        data = '|player|p1|foo|0\n|poke|p1|bar|item'
+        data = '\n'.join(['|player|p1|foo|0',
+                          '|player|p2|bar|0',
+                          '|poke|p1|eggs|item',
+                          '|switch|p1a: egg|eggs|100\/100'])
         service = BattleMetricsService()
         service.read_string(data=data)
-        assert_in('player_name', service.summary.columns)
-        assert_in('pokemon_name', service.summary.columns)
+        assert_in('player_name', service.summary.index.names)
+        assert_in('pokemon_name', service.summary.index.names)
