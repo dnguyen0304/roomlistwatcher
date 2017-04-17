92ac3e2f369e2bf56deabe2330731e9576a5be58
ubuntu <dnguyen0304@gmail.com>
2017-04-17 10:45:01 -0400

    ME-630: Changed to use Side object.

--- morpha/morpha/models/battle.py
+++ morpha/morpha/models/battle.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
 
 
+class Side(object):
+
+    def __init__(self, position, player):
+        self.position = position
+        self.player = player
+
+    def __repr__(self):
+        repr_ = '{}(position={}, player={})'
+        return repr_.format(self.__class__.__name__,
+                            self.position,
+                            self.player)
+
+
 class Battle(object):
 
     def __init__(self):
-        self.side_mapping = dict()
-
-    def get_player(self, position):
-        return self.side_mapping[position]
-
-    def get_all_players(self):
-        return self.side_mapping.values()
+        self._side_index = dict()
 
     def add_player(self, position, player):
-        self.side_mapping[position] = player
+        side = Side(position=position, player=player)
+        self._side_index[position] = side
+
+    def get_player(self, position):
+        return self._side_index[position].player
+
+    def get_all_players(self):
+        players = [side.player for side in self._side_index.values()]
+        return players
 
     def __repr__(self):
         repr_ = '{}()'
