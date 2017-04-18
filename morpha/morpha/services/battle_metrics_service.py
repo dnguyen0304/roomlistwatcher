1dc39637c1d237690074b31e4488ace503fd00f0
ubuntu <dnguyen0304@gmail.com>
2017-04-18 09:29:45 -0400

    ME-630: Added damage dealt metric.

--- morpha/morpha/services/battle_metrics_service.py
+++ morpha/morpha/services/battle_metrics_service.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import collections
-
 import pandas as pd
 
 from .. import models
@@ -10,7 +8,6 @@ from .. import models
 class BattleMetricsService(object):
 
     def __init__(self):
-        self._pokemon_index = collections.defaultdict(dict)
         self._battle = models.Battle()
         self.summary = pd.DataFrame()
 
@@ -26,19 +23,40 @@ class BattleMetricsService(object):
         for log_record in log_records:
             self._battle.apply_log_record(log_record)
             # While there is a pd.Index.any method, pd.MultiIndex
-            # objects do not support any type of truth testing. You
-            # must instead rely on the isinstance or type functions.
+            # objects do not support truth testing. You must instead
+            # rely on the isinstance or type functions.
             summary_has_index = isinstance(self.summary, pd.MultiIndex)
             if not summary_has_index and self._battle.pokemon_are_loaded:
                 self.summary = self._create_index()
+                self.summary = self._create_metrics()
+            if isinstance(log_record, models.DamageRecord):
+                self.summary = self._update_damage_dealt(log_record=log_record)
 
     def _create_index(self):
         tuples = list()
         for player in self._battle.get_all_players():
             for pokemon in player.pokemon:
                 tuples.append((player.name, pokemon.name))
-                self._pokemon_index[player.name][pokemon.name] = pokemon
         names = ['player_name', 'pokemon_name']
         index = pd.MultiIndex.from_tuples(tuples, names=names)
         summary = pd.DataFrame(index=index)
+        return summary
+
+    def _create_metrics(self):
+        summary = self.summary.copy()
+        summary.loc[:, 'damage_dealt'] = 0
+        return summary
+
+    def _update_damage_dealt(self, log_record):
+        summary = self.summary.copy()
+
+        current_action = self._battle.current_action
+        hit_points_before = current_action.targeted_pokemon.remaining_hit_points
+        hit_points_after = log_record.remaining_hit_points
+        hit_points_delta = hit_points_before - hit_points_after
+
+        index = (current_action.used_by_player.name,
+                 current_action.used_by_pokemon.name)
+        summary.loc[index, 'damage_dealt'] += hit_points_delta
+
         return summary
--- morpha/morpha/services/tests/test_battle_metrics_service.py
+++ morpha/morpha/services/tests/test_battle_metrics_service.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from nose.tools import assert_in
+from nose.tools import assert_equal, assert_in
 
 from .. import BattleMetricsService
 
@@ -14,10 +14,23 @@ class TestBattleMetricsService(object):
         data = '\n'.join(['|player|p1|foo|0',
                           '|player|p2|bar|0',
                           '|poke|p1|eggs|item',
-                          '|switch|p1a: egg|eggs|100\/100'])
+                          '|poke|p2|ham|item',
+                          '|switch|p1a: eggs|eggs|100\/100',
+                          '|switch|p2a: ham|ham|100\/100',
+                          '|move|p1a: eggs|foobar|p2a: ham',
+                          '|-damage|p2a: ham|50\/100',
+                          '|move|p1a: eggs|foobar|p2a: ham',
+                          '|-damage|p2a: ham|10\/100'])
         self.service = BattleMetricsService()
         self.service.read_string(data=data)
 
     def test_index(self):
         assert_in('player_name', self.service.summary.index.names)
         assert_in('pokemon_name', self.service.summary.index.names)
+
+    def test_metrics(self):
+        assert_in('damage_dealt', self.service.summary.columns)
+
+    def test_damage_dealt(self):
+        damage_dealt = self.service.summary.loc[('foo', 'eggs'), 'damage_dealt']
+        assert_equal(damage_dealt, 90)
