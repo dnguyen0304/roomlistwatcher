b27d49889b17be94274acf297169f070ec1aaa45
ubuntu <dnguyen0304@gmail.com>
2017-04-17 21:48:20 -0400

    ME-630: Added Pokemon cache.

--- morpha/morpha/services/battle_metrics_service.py
+++ morpha/morpha/services/battle_metrics_service.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import itertools
+import collections
 
 import pandas as pd
 
@@ -10,6 +10,7 @@ from .. import models
 class BattleMetricsService(object):
 
     def __init__(self):
+        self._pokemon_index = collections.defaultdict(dict)
         self._battle = models.Battle()
         self.summary = pd.DataFrame()
 
@@ -24,15 +25,16 @@ class BattleMetricsService(object):
     def _handle_log_records(self, log_records):
         for log_record in log_records:
             self._battle.apply_log_record(log_record)
-            if self._battle.pokemon_are_loaded:
-                index = self._construct_index()
-                self.summary = pd.DataFrame(index=index)
+            if not self.summary.index.any() and self._battle.pokemon_are_loaded:
+                self.summary = self._create_index()
 
-    def _construct_index(self):
+    def _create_index(self):
         tuples = list()
         for player in self._battle.get_all_players():
-            pokemon_names = (pokemon.name for pokemon in player.pokemon)
-            tuples.extend(itertools.product([player.name], pokemon_names))
+            for pokemon in player.pokemon:
+                tuples.append((player.name, pokemon.name))
+                self._pokemon_index[player.name][pokemon.name] = pokemon
         names = ['player_name', 'pokemon_name']
         index = pd.MultiIndex.from_tuples(tuples, names=names)
-        return index
+        summary = pd.DataFrame(index=index)
+        return summary
--- morpha/morpha/services/tests/test_battle_metrics_service.py
+++ morpha/morpha/services/tests/test_battle_metrics_service.py
@@ -7,12 +7,17 @@ from .. import BattleMetricsService
 
 class TestBattleMetricsService(object):
 
-    def test_summary_fields(self):
+    def __init__(self):
+        self.service = None
+
+    def setup(self):
         data = '\n'.join(['|player|p1|foo|0',
                           '|player|p2|bar|0',
                           '|poke|p1|eggs|item',
                           '|switch|p1a: egg|eggs|100\/100'])
-        service = BattleMetricsService()
-        service.read_string(data=data)
-        assert_in('player_name', service.summary.index.names)
-        assert_in('pokemon_name', service.summary.index.names)
+        self.service = BattleMetricsService()
+        self.service.read_string(data=data)
+
+    def test_index(self):
+        assert_in('player_name', self.service.summary.index.names)
+        assert_in('pokemon_name', self.service.summary.index.names)
