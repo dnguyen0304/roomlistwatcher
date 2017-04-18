50c605676c243517d57206a3ec23c35933283d11
ubuntu <dnguyen0304@gmail.com>
2017-04-18 11:04:52 -0400

    ME-630: -

--- morpha/morpha/models/tests/test_move_record.py
+++ morpha/morpha/models/tests/test_move_record.py
@@ -8,13 +8,13 @@ from .. import MoveRecord
 class TestMoveRecord(object):
 
     def test_from_message(self):
-        message = '|move|p1a: foo|foobar|p2a: bar'
+        message = '|move|p1a: eggs|foobar|p2a: ham'
         record = MoveRecord.from_message(message)
         assert_equal(record.used_by_position, 1)
-        assert_equal(record.used_by_pokemon_name, 'foo')
+        assert_equal(record.used_by_pokemon_name, 'eggs')
         assert_equal(record.move_name, 'foobar')
         assert_equal(record.targeted_position, 2)
-        assert_equal(record.targeted_pokemon_name, 'bar')
+        assert_equal(record.targeted_pokemon_name, 'ham')
 
     @raises(ValueError)
     def test_from_message_unmatched_pattern(self):
