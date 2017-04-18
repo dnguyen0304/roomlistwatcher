eefec5c99f079da45a2f74b13c9964e31d9c9d33
ubuntu <dnguyen0304@gmail.com>
2017-04-18 10:49:52 -0400

    ME-630: Added Pokemon.full_name attribute.

--- morpha/morpha/models/pokemon.py
+++ morpha/morpha/models/pokemon.py
@@ -3,11 +3,16 @@
 
 class Pokemon(object):
 
-    def __init__(self, name):
+    def __init__(self, name='', full_name=''):
+
+        if not name and not full_name:
+            raise TypeError('__init__() takes at least 2 arguments (1 given)')
+
         self.name = name
+        self.full_name = full_name
         self.remaining_hit_points = 0
         self.total_hit_points = 0
 
     def __repr__(self):
-        repr_ = '{}(name="{}")'
-        return repr_.format(self.__class__.__name__, self.name)
+        repr_ = '{}(name="{}", full_name="{}")'
+        return repr_.format(self.__class__.__name__, self.name, self.full_name)
