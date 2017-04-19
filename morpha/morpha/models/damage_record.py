5a3090ba4cf2b1ba5e5356b4818a64d3ff78c716
ubuntu <dnguyen0304@gmail.com>
2017-04-19 08:11:47 -0400

    ME-630: Removed DamageRecord.taken_by_* and DamageRecord.total_hit_points attributes.

--- morpha/morpha/models/damage_record.py
+++ morpha/morpha/models/damage_record.py
@@ -7,29 +7,18 @@ from . import IRecord
 
 class DamageRecord(IRecord):
 
-    def __init__(self,
-                 taken_by_position,
-                 taken_by_pokemon_name,
-                 remaining_hit_points,
-                 total_hit_points,
-                 indirectly_dealt_by):
+    def __init__(self, remaining_hit_points, indirectly_dealt_by=''):
 
         self.record_id = 0
 
-        self.taken_by_position = int(taken_by_position)
-        self.taken_by_pokemon_name = taken_by_pokemon_name
         self.remaining_hit_points = int(remaining_hit_points)
-        try:
-            self.total_hit_points = int(total_hit_points)
-        except TypeError:
-            self.total_hit_points = 0
         self.indirectly_dealt_by = indirectly_dealt_by or ''
 
     @classmethod
     def from_message(cls, message):
         pattern = ('\|-damage'
-                   '\|p(?P<taken_by_position>\d)a: (?P<taken_by_pokemon_name>[^\n\r|]+)'
-                   '\|(?P<remaining_hit_points>\d+)(?:\\\/(?P<total_hit_points>\d+))?(?: [^\n\r|]+)?'
+                   '\|p\da: [^\n\r|]+'
+                   '\|(?P<remaining_hit_points>\d+)(?:\\\/\d+)?(?: [^\n\r|]+)?'
                    '(?:\|\[from\] (?P<indirectly_dealt_by>[^\n\r|]+))?')
         match = re.match(pattern=pattern, string=message)
 
@@ -43,15 +32,9 @@ class DamageRecord(IRecord):
     def __repr__(self):
         repr_ = ('{}('
                  'record_id={}, '
-                 'taken_by_position={}, '
-                 'taken_by_pokemon_name="{}", '
                  'remaining_hit_points={}, '
-                 'total_hit_points={}, '
                  'indirectly_dealt_by="{}")')
         return repr_.format(self.__class__.__name__,
                             self.record_id,
-                            self.taken_by_position,
-                            self.taken_by_pokemon_name,
                             self.remaining_hit_points,
-                            self.total_hit_points,
                             self.indirectly_dealt_by)
