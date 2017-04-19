32a5bf5a15a2d8af0ac36e0b4d3399acd43594f8
ubuntu <dnguyen0304@gmail.com>
2017-04-19 15:09:00 -0400

    ME-630: Removed DamageRecord.indirectly_dealt_by attribute.

--- morpha/morpha/models/damage_record.py
+++ morpha/morpha/models/damage_record.py
@@ -7,19 +7,18 @@ from . import IRecord
 
 class DamageRecord(IRecord):
 
-    def __init__(self, remaining_hit_points, indirectly_dealt_by=''):
+    def __init__(self, remaining_hit_points):
 
         self.record_id = 0
 
         self.remaining_hit_points = int(remaining_hit_points)
-        self.indirectly_dealt_by = indirectly_dealt_by or ''
 
     @classmethod
     def from_message(cls, message):
         pattern = ('\|-damage'
                    '\|p\da: [^\n\r|]+'
                    '\|(?P<remaining_hit_points>\d+)(?:\\\/\d+)?(?: [^\n\r|]+)?'
-                   '(?:\|\[from\] (?P<indirectly_dealt_by>[^\n\r|]+))?')
+                   '(?:\|\[from\] [^\n\r|]+)?')
         match = re.match(pattern=pattern, string=message)
 
         if match:
@@ -30,11 +29,7 @@ class DamageRecord(IRecord):
         return record
 
     def __repr__(self):
-        repr_ = ('{}('
-                 'record_id={}, '
-                 'remaining_hit_points={}, '
-                 'indirectly_dealt_by="{}")')
+        repr_ = '{}(record_id={}, remaining_hit_points={})'
         return repr_.format(self.__class__.__name__,
                             self.record_id,
-                            self.remaining_hit_points,
-                            self.indirectly_dealt_by)
+                            self.remaining_hit_points)
--- morpha/morpha/models/tests/test_damage_record.py
+++ morpha/morpha/models/tests/test_damage_record.py
@@ -11,19 +11,16 @@ class TestDamageRecord(object):
         message = '|-damage|p1a: eggs|10\/100'
         record = DamageRecord.from_message(message)
         assert_equal(record.remaining_hit_points, 10)
-        assert_equal(record.indirectly_dealt_by, '')
 
     def test_from_message_faint_status_condition(self):
         message = '|-damage|p1a: eggs|0 fnt'
         record = DamageRecord.from_message(message)
         assert_equal(record.remaining_hit_points, 0)
-        assert_equal(record.indirectly_dealt_by, '')
 
     def test_from_message_indirect_damage(self):
         message = '|-damage|p1a: eggs|10\/100|[from] Stealth Rock'
         record = DamageRecord.from_message(message)
         assert_equal(record.remaining_hit_points, 10)
-        assert_equal(record.indirectly_dealt_by, 'Stealth Rock')
 
     @raises(ValueError)
     def test_from_message_incorrect_format(self):
