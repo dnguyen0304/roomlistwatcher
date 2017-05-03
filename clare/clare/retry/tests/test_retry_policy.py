a185b915d44e7584c8b92a80c91e49608efd316f
ubuntu <dnguyen0304@gmail.com>
2017-05-03 04:57:48 -0400

    ME-659: Refactored tests.

--- clare/clare/retry/tests/test_retry_policy.py
+++ clare/clare/retry/tests/test_retry_policy.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import mock
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_greater
 
-from .. import AfterAttempt, AfterNever, FixedWaitStrategy, RetryPolicy
+from .. import RetryPolicy, stop_strategies, wait_strategies
 
 
 class MockException(Exception):
@@ -39,45 +39,33 @@ class TestRetryPolicy(object):
         self.handled_exceptions = (MockException,)
 
     def test_execute_stop_after_success(self):
-        stop_strategy = AfterNever()
-        wait_time = 0
-        wait_strategy = FixedWaitStrategy(wait_time=wait_time)
-        retry_policy = RetryPolicy(stop_strategy=stop_strategy,
-                                   wait_strategy=wait_strategy)
+        retry_policy = RetryPolicy(
+            stop_strategy=stop_strategies.AfterNever(),
+            wait_strategy=wait_strategies.FixedWaitStrategy(wait_time=0))
         retry_policy.execute(callable=self.service.call)
         assert_equal(self.service.call_count, 1)
 
     def test_execute_wait(self):
-        maximum_attempt = 2
-        stop_strategy = AfterAttempt(maximum_attempt=maximum_attempt)
-        wait_time = None
-        wait_strategy = FixedWaitStrategy(wait_time=wait_time)
-
-        retry_policy = RetryPolicy(stop_strategy=stop_strategy,
-                                   wait_strategy=wait_strategy,
-                                   handled_exceptions=self.handled_exceptions)
-
         _sleep = mock.MagicMock()
+        retry_policy = RetryPolicy(
+            stop_strategy=stop_strategies.AfterAttempt(maximum_attempt=2),
+            wait_strategy=wait_strategies.FixedWaitStrategy(wait_time=None),
+            handled_exceptions=self.handled_exceptions)
         retry_policy.execute(callable=self.service.call_and_raise,
                              _sleep=_sleep)
         _sleep.assert_called()
 
     def test_execute_successful_attempt_returns_result(self):
-        stop_strategy = AfterNever()
-        wait_time = 0
-        wait_strategy = FixedWaitStrategy(wait_time=wait_time)
-        retry_policy = RetryPolicy(stop_strategy=stop_strategy,
-                                   wait_strategy=wait_strategy)
+        retry_policy = RetryPolicy(
+            stop_strategy=stop_strategies.AfterNever(),
+            wait_strategy=wait_strategies.FixedWaitStrategy(wait_time=0))
         result = retry_policy.execute(callable=self.service.call_and_return)
         assert_equal(result, 'foo')
 
     def test_execute_continue_on_handled_exceptions(self):
-        maximum_attempt = 2
-        stop_strategy = AfterAttempt(maximum_attempt=maximum_attempt)
-        wait_time = 0
-        wait_strategy = FixedWaitStrategy(wait_time=wait_time)
-        retry_policy = RetryPolicy(stop_strategy=stop_strategy,
-                                   wait_strategy=wait_strategy,
-                                   handled_exceptions=self.handled_exceptions)
+        retry_policy = RetryPolicy(
+            stop_strategy=stop_strategies.AfterAttempt(maximum_attempt=2),
+            wait_strategy=wait_strategies.FixedWaitStrategy(wait_time=0),
+            handled_exceptions=self.handled_exceptions)
         retry_policy.execute(callable=self.service.call_and_raise)
-        assert_equal(self.service.call_count, maximum_attempt)
+        assert_greater(self.service.call_count, 1)
