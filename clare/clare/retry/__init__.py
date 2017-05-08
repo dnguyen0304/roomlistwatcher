53a436337d52630cf3dac2adae6c765cd7b1db39
ubuntu <dnguyen0304@gmail.com>
2017-05-08 10:43:33 -0400

    ME-659: Added retry.Topic.

--- clare/clare/retry/__init__.py
+++ clare/clare/retry/__init__.py
@@ -3,9 +3,11 @@
 from . import exceptions
 from . import stop_strategies
 from . import wait_strategies
+from .policy import Topic
 from .policy_builder import PolicyBuilder
 
 __all__ = ['PolicyBuilder',
+           'Topic',
            'exceptions',
            'stop_strategies',
            'wait_strategies']
