22558d9729052f2880eccd47c60b78c2544c85d2
ubuntu <dnguyen0304@gmail.com>
2017-05-09 13:38:42 -0400

    ME-659: Changed docstrings.

--- clare/clare/retry/policy_builder.py
+++ clare/clare/retry/policy_builder.py
@@ -37,24 +37,6 @@ class PolicyBuilder(object):
           - 0 or more results on which to continue
           - 0 or more hooks
           - at most 1 messaging brokers
-
-        A "successful" attempt is understood as one where an exception
-        was not thrown within the callable.
-
-        Parameters
-        ----------
-        stop_strategies : iterable of IStopStrategy
-            Defaults to an empty list.
-        wait_strategy : IWaitStrategy
-            Defaults to None.
-        continue_strategies : iterable of IContinueStrategy
-            Continuing takes precedence over stopping after successful
-            attempts. In other words, it "overrides" those cases.
-            Defaults to an empty list.
-        handled_exceptions : iterable of Exception
-            Defaults to an empty tuple.
-        messaging_broker : clare.event_driven.messaging.Broker
-            See the method docstring for more details. Defaults to None.
         """
 
         self._stop_strategies = stop_strategies or list()
@@ -64,26 +46,75 @@ class PolicyBuilder(object):
         self._messaging_broker = messaging_broker
 
     def with_stop_strategy(self, stop_strategy):
+
+        """
+        Parameters
+        ----------
+        stop_strategy : clare.retry.stop_strategies.IStopStrategy
+        """
+
         self._stop_strategies.append(stop_strategy)
         return self
 
     def with_wait_strategy(self, wait_strategy):
+
+        """
+        Parameters
+        ----------
+        wait_strategy : clare.retry.wait_strategies.IWaitStrategy
+        """
+
         self._wait_strategy = wait_strategy
         return self
 
     def _with_continue_strategy(self, continue_strategy):
+
+        """
+        Parameters
+        ----------
+        continue_strategy : clare.retry.continue_strategies.IContinueStrategy
+        """
+
         self._continue_strategies.append(continue_strategy)
         return self
 
     def continue_on_exception(self, exception):
+
+        """
+        When the algorithm would normally stop due to an exception
+        being thrown within the callable, continuing overrides that
+        behavior.
+        """
+
         self._handled_exceptions += (exception,)
         return self
 
     def continue_if_result(self, predicate):
+
+        """
+        When the algorithm would normally stop due to a successful
+        attempt, continuing overrides that behavior.
+
+        A "successful" attempt is understood as one where an exception
+        was not thrown within the callable.
+
+        Parameters
+        ---------
+        predicate : collections.Callable
+            The predicate must accept one argument and return a Boolean.
+        """
+
         continue_strategy = continue_strategies.AfterResult(predicate=predicate)
         return self._with_continue_strategy(continue_strategy)
 
     def with_messaging_broker(self, messaging_broker):
+
+        """
+        Parameters
+        ----------
+        messaging_broker : clare.event_driven.messaging.Broker
+        """
+
         self._messaging_broker = messaging_broker
         return self
 
@@ -109,7 +140,8 @@ class PolicyBuilder(object):
         Parameters
         ----------
         predicate : collections.Callable
-            The predicate must accept one argument of type str.
+            The predicate must accept one argument of type str and
+            return a Boolean.
         topic : enum.Enum
         """
 
