c094b9fe0371acccd53443d86e6620133e4e00b4
ubuntu <dnguyen0304@gmail.com>
2017-05-13 09:58:21 -0400

    ME-637: Added generate_id_strategy dependency.

--- clare/clare/scraper/repositories.py
+++ clare/clare/scraper/repositories.py
@@ -2,6 +2,10 @@
 
 import random
 import string
+import sys
+
+if sys.version_info[:2] == (2, 7):
+    range = xrange
 
 from . import interfaces
 
@@ -10,13 +14,31 @@ class EntityNotFound(Exception):
     pass
 
 
+def random_alphanumeric_strings(length):
+
+    valid_characters = string.ascii_letters + string.digits
+    while True:
+        sid = ''.join(random.SystemRandom().choice(valid_characters)
+                      for _
+                      in range(length))
+        yield sid
+
+
 class Default(interfaces.IRepository):
 
-    def __init__(self):
+    def __init__(self, generate_id_strategy):
+
+        """
+        Parameters
+        ----------
+        generate_id_strategy : collections.Iterable
+        """
+
+        self._generate_id_strategy = generate_id_strategy
         self._entities = dict()
 
     def add(self, entity):
-        entity_id = self.generate_entity_id()
+        entity_id = next(self._generate_id_strategy)
         self._entities[entity_id] = entity
         return entity_id
 
@@ -27,10 +49,7 @@ class Default(interfaces.IRepository):
             raise EntityNotFound
         return entity
 
-    @staticmethod
-    def generate_entity_id():
-        valid_characters = string.ascii_letters + string.digits
-        sid = ''.join(random.SystemRandom().choice(valid_characters)
-                      for _
-                      in range(32))
-        return sid
+    def __repr__(self):
+        repr_ = '{}(generate_id_strategy={})'
+        return repr_.format(self.__class__.__name__,
+                            self._generate_id_strategy)
--- clare/clare/scraper/tests/test_repositories.py
+++ clare/clare/scraper/tests/test_repositories.py
@@ -1,8 +1,38 @@
 # -*- coding: utf-8 -*-
 
+import types
+
 from nose.tools import assert_equal, assert_is_instance, assert_true, raises
 
 from .. import repositories
+
+
+def test_random_alphanumeric_strings_is_generator():
+
+    length = None
+    output = repositories.random_alphanumeric_strings(length=length)
+    assert_is_instance(output, types.GeneratorType)
+
+
+def test_random_alphanumeric_strings_return_value_is_of_type_string():
+
+    length = 0
+    id = next(repositories.random_alphanumeric_strings(length=length))
+    assert_is_instance(id, str)
+
+
+def test_random_alphanumeric_strings_return_value_has_correct_length():
+
+    length = 1
+    id = next(repositories.random_alphanumeric_strings(length=length))
+    assert_equal(len(id), length)
+
+
+def test_random_alphanumeric_strings_return_value_is_alphanumeric():
+
+    length = 1
+    id = next(repositories.random_alphanumeric_strings(length=length))
+    assert_true(id.isalnum())
 
 
 class TestDefault:
@@ -11,7 +41,11 @@ class TestDefault:
         self.repository = None
 
     def setup(self):
-        self.repository = repositories.Default()
+        length = 32
+        generate_id_strategy = repositories.random_alphanumeric_strings(
+            length=length)
+        self.repository = repositories.Default(
+            generate_id_strategy=generate_id_strategy)
 
     def test_get_return_value_is_same_entity(self):
         input_entity = expected_entity = 'foo'
@@ -22,21 +56,3 @@ class TestDefault:
     @raises(repositories.EntityNotFound)
     def test_get_raises_entity_not_found(self):
         self.repository.get(entity_id=None)
-
-
-def test_generate_entity_id_return_value_is_of_type_string():
-
-    entity_id = repositories.Default.generate_entity_id()
-    assert_is_instance(entity_id, str)
-
-
-def test_generate_entity_id_return_value_is_32_characters_long():
-
-    entity_id = repositories.Default.generate_entity_id()
-    assert_true(len(entity_id) == 32)
-
-
-def test_generate_entity_id_return_value_is_alphanumeric():
-
-    entity_id = repositories.Default.generate_entity_id()
-    assert_true(entity_id.isalnum())
