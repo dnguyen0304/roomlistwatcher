f603a6fa988dc1375b5fcceb00c91891acf62af8
ubuntu <dnguyen0304@gmail.com>
2017-05-14 10:38:24 -0400

    ME-637: Added clare.common.logging.

--- clare/clare/common/__init__.py
+++ clare/clare/common/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import event_driven
+from . import logging
 from . import retry
 
 __all__ = ['event_driven',
+           'logging',
            'retry']
---
+++ clare/clare/common/logging/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import interfaces
+from .event import Event
+
+__all__ = ['Event',
+           'interfaces']
