cd92908f78dcf0e5e72051da883d2ac945f4431c
ubuntu <dnguyen0304@gmail.com>
2017-05-29 10:43:06 -0400

    ME-653: Added Polling scraper decorator.

--- clare/clare/watching/scraping/decorators.py
+++ clare/clare/watching/scraping/decorators.py
@@ -1,6 +1,53 @@
 # -*- coding: utf-8 -*-
 
+import time
+
 from . import interfaces
+
+
+class Polling(object):
+
+    def __init__(self, scraper, wait_time, buffer):
+
+        """
+        Change the scrape behavior to poll the web page instead.
+
+        Parameters
+        ----------
+        scraper : clare.watching.scraping.interfaces.IDisposable and
+                  clare.watching.scraping.interfaces.IScraper
+        wait_time : float
+            Wait time in seconds.
+        buffer : Queue.Queue
+        """
+
+        self._scraper = scraper
+        self.wait_time = wait_time
+        self._buffer = buffer
+
+    def scrape(self, url, _sleep=time.sleep):
+
+        """
+        Parameters
+        ----------
+        url : str
+            Web page URL.
+        _sleep : collections.Callable
+            Used internally. Defaults to time.sleep.
+        """
+
+        while True:
+            data = self._scraper.scrape(url=url)
+            for item in data:
+                self._buffer.put(item=item)
+            _sleep(self.wait_time)
+
+    def __repr__(self):
+        repr_ = '{}(scraper={}, wait_time={}, buffer={})'
+        return repr_.format(self.__class__.__name__,
+                            self._scraper,
+                            self.wait_time,
+                            self._buffer)
 
 
 class SerializedElements(interfaces.IDisposable):
