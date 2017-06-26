a74ca3d643ed16068d4339fb226b1f533abc1bcd
ubuntu <dnguyen0304@gmail.com>
2017-06-26 16:47:57 -0400

    N/A: Changed code layout.

--- clare/clare/application/download_bot/replay_downloaders.py
+++ clare/clare/application/download_bot/replay_downloaders.py
@@ -77,6 +77,34 @@ class ReplayDownloader(interfaces.IReplayDownloader):
                             self._wait_context)
 
 
+class Retrying(interfaces.IReplayDownloader):
+
+    def __init__(self, replay_downloader, policy):
+
+        """
+        Parameters
+        ----------
+        replay_downloader : clare.application.download_bot.interfaces.IReplayDownloader
+        policy : clare.common.retry.policy.Policy
+        """
+
+        self._replay_downloader = replay_downloader
+        self._policy = policy
+
+    def run(self, url):
+        download = functools.partial(self._replay_downloader.run, url=url)
+        self._policy.execute(download)
+
+    def dispose(self):
+        self._replay_downloader.dispose()
+
+    def __repr__(self):
+        repr_ = '{}(replay_downloader={}, policy={})'
+        return repr_.format(self.__class__.__name__,
+                            self._replay_downloader,
+                            self._policy)
+
+
 class Validating(interfaces.IReplayDownloader):
 
     def __init__(self, replay_downloader, validator):
@@ -126,31 +154,3 @@ class Validating(interfaces.IReplayDownloader):
         return repr_.format(self.__class__.__name__,
                             self._replay_downloader,
                             self._validator)
-
-
-class Retrying(interfaces.IReplayDownloader):
-
-    def __init__(self, replay_downloader, policy):
-
-        """
-        Parameters
-        ----------
-        replay_downloader : clare.application.download_bot.interfaces.IReplayDownloader
-        policy : clare.common.retry.policy.Policy
-        """
-
-        self._replay_downloader = replay_downloader
-        self._policy = policy
-
-    def run(self, url):
-        download = functools.partial(self._replay_downloader.run, url=url)
-        self._policy.execute(download)
-
-    def dispose(self):
-        self._replay_downloader.dispose()
-
-    def __repr__(self):
-        repr_ = '{}(replay_downloader={}, policy={})'
-        return repr_.format(self.__class__.__name__,
-                            self._replay_downloader,
-                            self._policy)
