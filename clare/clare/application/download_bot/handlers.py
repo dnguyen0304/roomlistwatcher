3f44851048ce95a7a8986ffe234ea7fdd5662909
ubuntu <dnguyen0304@gmail.com>
2017-06-26 17:13:10 -0400

    N/A: Added Orchestrating handler.

--- clare/clare/application/download_bot/handlers.py
+++ clare/clare/application/download_bot/handlers.py
@@ -2,6 +2,10 @@
 
 from __future__ import print_function
 
+import collections
+
+from . import exceptions
+from .topic import Topic
 from clare import common
 
 
@@ -25,6 +29,40 @@ class Download(common.messaging.client.consumer.internals.interfaces.IHandler):
     def __repr__(self):
         repr_ = '{}(download_bot={})'
         return repr_.format(self.__class__.__name__, self._download_bot)
+
+
+class Orchestrating(common.messaging.client.consumer.internals.interfaces.IHandler):
+
+    def __init__(self, handler, logger):
+
+        """
+        Parameters
+        ----------
+        handler : clare.application.download_bot.handlers.Download
+        logger : logging.Logger
+        """
+
+        self._handler = handler
+        self._logger = logger
+
+    def handle(self, record):
+        try:
+            file_path = self._handler.handle(record=record)
+        except (exceptions.RoomExpired, common.retry.exceptions.MaximumRetry) as e:
+            message = common.logging.utilities.format_exception(e=e)
+            self._logger.debug(msg=message)
+        else:
+            arguments = collections.OrderedDict()
+            arguments['file_path'] = file_path
+            event = common.logging.Event(topic=Topic.REPLAY_DOWNLOADED,
+                                         arguments=arguments)
+            self._logger.info(msg=event.to_json())
+
+    def __repr__(self):
+        repr_ = '{}(handler={}, logger={})'
+        return repr_.format(self.__class__.__name__,
+                            self._handler,
+                            self._logger)
 
 
 class Printing(common.messaging.client.consumer.internals.interfaces.IHandler):
