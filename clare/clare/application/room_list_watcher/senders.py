afcc2eb73a71d97de5232b32693073760184e23a
ubuntu <dnguyen0304@gmail.com>
2017-06-27 18:00:04 -0400

    ME-653: Added Logging sender.

---
+++ clare/clare/application/room_list_watcher/senders.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import collections
+
+from . import topics
+from clare import common
+
+
+class Logging(object):
+
+    def __init__(self, sender, logger):
+
+        """
+        Parameters
+        ----------
+        sender : clare.common.messaging.producer.senders.Sender
+        logger : logging.Logger
+        """
+
+        self._sender = sender
+        self._logger = logger
+
+    def push(self, record, timeout):
+
+        """
+        Parameters
+        ----------
+        record : clare.common.messaging.records.Record
+        timeout : float
+        """
+
+        self._sender.push(record=record, timeout=timeout)
+
+        arguments = collections.OrderedDict()
+        arguments['path'] = record.value
+        event = common.logging.Event(topic=topics.Topic.ROOM_CREATED,
+                                     arguments=arguments)
+        message = event.to_json()
+        self._logger.info(msg=message)
+
+    def __repr__(self):
+        repr_ = '{}(sender={}, logger={})'
+        return repr_.format(self.__class__.__name__,
+                            self._sender,
+                            self._logger)
