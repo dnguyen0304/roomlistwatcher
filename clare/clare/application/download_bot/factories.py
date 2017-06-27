93715046db94cd3f4c5454fa0be38eb991b75eeb
ubuntu <dnguyen0304@gmail.com>
2017-06-26 22:26:49 -0400

    N/A: Changed Factory to include messaging.

--- clare/clare/application/download_bot/factories.py
+++ clare/clare/application/download_bot/factories.py
@@ -14,8 +14,68 @@ from . import exceptions
 from . import filters
 from . import handlers
 from . import replay_downloaders
+from . import subscribers
 from clare import common
 from clare.common.messaging.client import consumer
+
+
+class _MessagingBrokerFactory(object):
+
+    def __init__(self, properties):
+
+        """
+        Parameters
+        ----------
+        properties : collections.Mapping
+        """
+
+        self._properties = properties
+
+    def create(self, event_name):
+
+        """
+        Parameters
+        ----------
+        event_name : str
+
+        Returns
+        -------
+        common.event_driven.messaging.Broker
+        """
+
+        # Construct the attempt started subscriber with logging.
+        attempt_started_subscriber = subscribers.AttemptStarted(
+            event_name=event_name)
+        attempt_started_subscriber = subscribers.Logging(
+            subscriber=attempt_started_subscriber,
+            logger=logging.getLogger(name=self._properties['handler']['logger']['name']))
+
+        # Construct the attempt completed subscriber with logging.
+        attempt_completed_subscriber = subscribers.AttemptCompleted(
+            event_name=event_name)
+        attempt_completed_subscriber = subscribers.Logging(
+            subscriber=attempt_completed_subscriber,
+            logger=logging.getLogger(name=self._properties['handler']['logger']['name']))
+
+        # Construct the messaging broker.
+        messaging_broker = common.event_driven.messaging.Broker(
+            observable_class=common.event_driven.Observable)
+
+        # Initialize the messaging broker with topics and subscribers.
+        messaging_broker.create_topic(name=common.retry.Topic.ATTEMPT_STARTED.name)
+        messaging_broker.create_topic(name=common.retry.Topic.ATTEMPT_COMPLETED.name)
+        messaging_broker.subscribe(
+            subscriber=attempt_started_subscriber,
+            topic_name=common.retry.Topic.ATTEMPT_STARTED.name)
+        messaging_broker.subscribe(
+            subscriber=attempt_completed_subscriber,
+            topic_name=common.retry.Topic.ATTEMPT_COMPLETED.name)
+
+        return messaging_broker
+
+    def __repr__(self):
+        repr_ = '{}(properties={})'
+        return repr_.format(self.__class__.__name__, self._properties)
 
 
 class Factory(object):
@@ -29,6 +89,8 @@ class Factory(object):
         """
 
         self._properties = properties
+        self._messaging_broker_factory = _MessagingBrokerFactory(
+            properties=properties)
 
     def create(self):
         directory_path = os.path.join(
@@ -59,11 +121,14 @@ class Factory(object):
             maximum_duration=self._properties['replay_downloader']['policy']['stop_strategy']['maximum_duration'])
         wait_strategy = common.retry.wait_strategies.Fixed(
             wait_time=self._properties['replay_downloader']['policy']['wait_strategy']['wait_time'])
+        messaging_broker = self._messaging_broker_factory.create(
+            event_name='ReplayDownload')
         policy = common.retry.PolicyBuilder() \
             .with_stop_strategy(stop_strategy) \
             .with_wait_strategy(wait_strategy) \
             .continue_on_exception(exceptions.ConnectionLost) \
             .continue_on_exception(exceptions.BattleNotCompleted) \
+            .with_messaging_broker(messaging_broker) \
             .build()
         replay_downloader = replay_downloaders.Retrying(
             replay_downloader=replay_downloader,
@@ -76,9 +141,12 @@ class Factory(object):
             maximum_duration=self._properties['download_validator']['policy']['stop_strategy']['maximum_duration'])
         wait_strategy = common.retry.wait_strategies.Fixed(
             wait_time=self._properties['download_validator']['policy']['wait_strategy']['wait_time'])
+        messaging_broker = self._messaging_broker_factory.create(
+            event_name='DownloadValidate')
         policy_builder = common.retry.PolicyBuilder() \
             .with_stop_strategy(stop_strategy) \
-            .with_wait_strategy(wait_strategy)
+            .with_wait_strategy(wait_strategy) \
+            .with_messaging_broker(messaging_broker)
         # Exclude intermediary and temporary files.
         policy_builder = policy_builder \
             .continue_if_result(predicate=lambda x: x == '') \
