7b16e0dbdebbfd5043e636208b74ac389449e058
ubuntu <dnguyen0304@gmail.com>
2017-06-27 18:04:24 -0400

    ME-653: Added IFlushStrategy interface.

--- clare/clare/application/room_list_watcher/interfaces.py
+++ clare/clare/application/room_list_watcher/interfaces.py
@@ -3,6 +3,27 @@
 import abc
 
 
+class IFlushStrategy(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def should_flush(self, collection):
+
+        """
+        Parameters
+        ----------
+        collection : typing.Any
+
+        Returns
+        -------
+        bool
+            True if the collection should be flushed.
+        """
+
+        pass
+
+
 class IScraper(object):
 
     __metaclass__ = abc.ABCMeta
