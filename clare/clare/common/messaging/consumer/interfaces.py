4e8b049c14d100a988ce720c38fac767c672bd59
ubuntu <dnguyen0304@gmail.com>
2017-06-28 16:36:02 -0400

    N/A: Added IFetcher interface.

--- clare/clare/common/messaging/consumer/interfaces.py
+++ clare/clare/common/messaging/consumer/interfaces.py
@@ -3,6 +3,26 @@
 import abc
 
 
+class IFetcher(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def pop(self, timeout):
+
+        """
+        Parameters
+        ----------
+        timeout : float
+
+        Returns
+        -------
+        clare.common.messaging.records.Record
+        """
+
+        pass
+
+
 class IHandler(object):
 
     __metaclass__ = abc.ABCMeta
