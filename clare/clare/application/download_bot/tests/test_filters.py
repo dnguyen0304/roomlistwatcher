7bbae4cec9b70ea8244628605387a1c0c0632f8c
ubuntu <dnguyen0304@gmail.com>
2017-06-28 14:27:04 -0400

    N/A: Fixed tests after messaging move.

--- clare/clare/application/download_bot/tests/test_filters.py
+++ clare/clare/application/download_bot/tests/test_filters.py
@@ -16,17 +16,17 @@ class TestOnlyGenerationSevenMetagame(object):
 
     def test_does_not_filter_generation_seven_metagame_battle(self):
         value = '/battle-gen7foo-0'
-        input = messaging.client.records.Record(queue_name=None,
-                                                timestamp=None,
-                                                value=value)
+        input = messaging.records.Record(queue_name=None,
+                                         timestamp=None,
+                                         value=value)
         output = self.filter.filter(record=input)
         assert_is(output, input)
 
     def test_does_filter_non_generation_seven_metagame_battle(self):
         value = '/battle-gen0foo-0'
-        record = messaging.client.records.Record(queue_name=None,
-                                                 timestamp=None,
-                                                 value=value)
+        record = messaging.records.Record(queue_name=None,
+                                          timestamp=None,
+                                          value=value)
         record = self.filter.filter(record=record)
         assert_is_none(record)
 
@@ -41,16 +41,16 @@ class TestOnlyOverusedMetagame(object):
 
     def test_does_not_filter_overused_metagame_battle(self):
         value = '/battle-fooou-0'
-        input = messaging.client.records.Record(queue_name=None,
-                                                timestamp=None,
-                                                value=value)
+        input = messaging.records.Record(queue_name=None,
+                                         timestamp=None,
+                                         value=value)
         output = self.filter.filter(record=input)
         assert_is(output, input)
 
     def test_does_filter_non_overused_metagame_battle(self):
         value = '/battle-foobar-0'
-        record = messaging.client.records.Record(queue_name=None,
-                                                 timestamp=None,
-                                                 value=value)
+        record = messaging.records.Record(queue_name=None,
+                                          timestamp=None,
+                                          value=value)
         record = self.filter.filter(record=record)
         assert_is_none(record)
