a85acbdc44d9196b54f4540e8d0e5b301515b2ea
ubuntu <dnguyen0304@gmail.com>
2017-06-28 12:17:02 -0400

    ME-653: Added Producer factory.
    Changed import style.

--- clare/clare/application/room_list_watcher/factories.py
+++ clare/clare/application/room_list_watcher/factories.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import sys
-
-if sys.version_info[:2] == (2, 7):
-    import Queue as queue
 
 import selenium.webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 from . import exceptions
+from . import filters
+from . import flush_strategies
 from . import record_factories
 from . import scrapers
+from . import senders
 from clare import common
+from clare.common import automation
+from clare.common import messaging
+from clare.common import retry
 
 
 class Factory(object):
@@ -45,24 +47,24 @@ class Factory(object):
         wait_context = WebDriverWait(
             web_driver,
             timeout=self._properties['scraper']['validator']['wait_context']['timeout'])
-        validator = common.automation.validators.PokemonShowdown(
+        validator = automation.validators.PokemonShowdown(
             wait_context=wait_context)
         scraper = scrapers.Validating(scraper=scraper, validator=validator)
 
         # Include retrying.
-        stop_strategy = common.retry.stop_strategies.AfterAttempt(
+        stop_strategy = retry.stop_strategies.AfterAttempt(
             maximum_attempt=self._properties['scraper']['policy']['stop_strategy']['maximum_attempt'])
-        wait_strategy = common.retry.wait_strategies.Fixed(
+        wait_strategy = retry.wait_strategies.Fixed(
             wait_time=self._properties['scraper']['policy']['wait_strategy']['wait_time'])
         logger = logging.getLogger(name=self._properties['scraper']['policy']['messaging_broker']['logger']['name'])
-        messaging_broker_factory = common.retry.messaging.broker_factories.Logging(
+        messaging_broker_factory = retry.messaging.broker_factories.Logging(
             logger=logger)
         messaging_broker = messaging_broker_factory.create(
-            event_name='RoomListScrape')
-        policy = common.retry.PolicyBuilder() \
+            event_name='ROOM_LIST_SCRAPE')
+        policy = retry.PolicyBuilder() \
             .with_stop_strategy(stop_strategy) \
             .with_wait_strategy(wait_strategy) \
-            .continue_on_exception(common.automation.exceptions.ConnectionLost) \
+            .continue_on_exception(automation.exceptions.ConnectionLost) \
             .continue_on_exception(exceptions.InitializationFailed) \
             .continue_on_exception(exceptions.ExtractFailed) \
             .with_messaging_broker(messaging_broker) \
@@ -80,24 +82,59 @@ class Factory(object):
         scraper = scrapers.RecordMarshallingDecorator(scraper=scraper,
                                                       factory=record_factory)
 
-        # Include queuing.
-        message_queue = queue.Queue()
-        scraper = scrapers.QueuingDecorator(scraper=scraper,
-                                            message_queue=message_queue)
-
-        # Include polling.
-        scraper = scrapers.PollingDecorator(
-            scraper=scraper,
-            wait_time=self._properties['scraper']['wait_time'])
-
         # Include orchestration.
         logger = logging.getLogger(name=self._properties['scraper']['logger']['name'])
-        scraper = scrapers.OrchestratingDecorator(
-            scraper=scraper,
-            logger=logger)
+        scraper = scrapers.Orchestrating(scraper=scraper, logger=logger)
 
         return scraper
 
     def __repr__(self):
         repr_ = '{}(properties={})'
         return repr_.format(self.__class__.__name__, self._properties)
+
+
+class Producer(object):
+
+    def __init__(self, properties, sender):
+
+        """
+        Parameters
+        ----------
+        properties : collections.Mapping
+        sender : clare.common.messaging.producer.senders.Sender
+        """
+
+        self._factory = Factory(properties=properties)
+        self._properties = properties
+        self._sender = sender
+
+    def create(self):
+        # Construct the source.
+        scraper = self._factory.create()
+        source = scrapers.SourceAdapter(scraper=scraper, url=self._properties['scraper']['url'])
+
+        # Construct the sender.
+        sender = self._sender
+        # Include logging.
+        logger = logging.getLogger(name=self._properties['sender']['logger']['name'])
+        sender = senders.Logging(sender=sender, logger=logger)
+
+        # Construct the no duplicate filter.
+        after_duration = flush_strategies.AfterDuration(
+            maximum_duration=self._properties['filter']['flush_strategy']['maximum_duration'])
+        no_duplicate = filters.NoDuplicate(flush_strategy=after_duration)
+
+        # Construct the producer.
+        producer_ = messaging.producer.builders.Builder() \
+            .with_source(source) \
+            .with_sender(sender) \
+            .with_filter(no_duplicate) \
+            .build()
+
+        return producer_
+
+    def __repr__(self):
+        repr_ = '{}(properties={}, sender={})'
+        return repr_.format(self.__class__.__name__,
+                            self._properties,
+                            self._sender)
