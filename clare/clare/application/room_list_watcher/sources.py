8aa311e9b8f46888db35e9ec466588f14923e409
ubuntu <dnguyen0304@gmail.com>
2017-06-28 21:43:00 -0400

    N/A: Added Deque source.

---
+++ clare/clare/application/room_list_watcher/sources.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from clare.common import messaging
+
+
+class Deque(messaging.producer.interfaces.ISource):
+
+    def __init__(self, deque, record_factory):
+
+        """
+        Parameters
+        ----------
+        deque : collections.deque
+        record_factory : clare.application.room_list_watcher.record_factories.RecordFactory
+        """
+
+        self._deque = deque
+        self._record_factory = record_factory
+
+    def emit(self):
+        try:
+            value = self._deque.popleft()
+        except IndexError:
+            raise messaging.producer.exceptions.Timeout
+        else:
+            record = self._record_factory.create(value=value)
+            return record
+
+    def __repr__(self):
+        repr_ = '{}(deque={}, record_factory={})'
+        return repr_.format(self.__class__.__name__,
+                            self._deque,
+                            self._record_factory)
