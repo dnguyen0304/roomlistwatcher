bbfa9d505029cca927e4f1f97fda389ea5ab0b33
ubuntu <dnguyen0304@gmail.com>
2017-07-10 12:47:46 -0400

    ME-715: Removed unused Fetcher.

--- clare/clare/common/messaging/consumer/__init__.py
+++ clare/clare/common/messaging/consumer/__init__.py
@@ -2,10 +2,8 @@
 
 from . import consumers
 from . import exceptions
-from . import fetchers
 from . import interfaces
 
 __all__ = ['consumers',
            'exceptions',
-           'fetchers',
            'interfaces']
