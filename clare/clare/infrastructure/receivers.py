9c1c4204b20810f6f74b0da07a9ef99ca8b83220
ubuntu <dnguyen0304@gmail.com>
2017-08-08 18:01:52 -0400

    Added Queue receiver.

--- clare/clare/infrastructure/receivers.py
+++ clare/clare/infrastructure/receivers.py
@@ -7,6 +7,102 @@ from clare.common.messaging import consumer
 BATCH_SIZE_MINIMUM_COUNT = 1
 
 
+class Queue(consumer.receivers.Buffering):
+
+    def __init__(self,
+                 batch_size_maximum_count,
+                 countdown_timer,
+                 message_factory,
+                 batch_size_minimum_count=BATCH_SIZE_MINIMUM_COUNT,
+                 _queue=None,
+                 _buffer=None):
+
+        """
+        Parameters
+        ----------
+        batch_size_maximum_count : int
+            Maximum size of the batch. The units are in number of
+            messages.
+        batch_size_minimum_count : int
+            Minimum size of the batch. The units are in number of
+            messages. Defaults to BATCH_SIZE_MINIMUM_COUNT.
+        countdown_timer : clare.common.utilities.timers.CountdownTimer
+        message_factory : clare.common.messaging.factories.Message
+        """
+
+        self._original_batch_size_maximum_count = batch_size_maximum_count
+        self._original_batch_size_minimum_count = batch_size_minimum_count
+        self._countdown_timer = countdown_timer
+        self._message_factory = message_factory
+
+        # Deques are thread-safe.
+        self._queue = _queue if _queue is not None else collections.deque()
+        self._buffer = _buffer if _buffer is not None else collections.deque()
+
+        self._current_batch_size_maximum_count = batch_size_maximum_count
+        self._current_batch_size_minimum_count = batch_size_minimum_count
+
+    def receive(self):
+        if not self._buffer:
+            self._fill_buffer()
+        try:
+            message = self._buffer.popleft()
+        except IndexError:
+            message = 'The receive operation timed out.'
+            raise consumer.exceptions.ReceiveTimeout(message)
+        return message
+
+    def _fill_buffer(self):
+        messages = list()
+
+        self._countdown_timer.start()
+
+        while True:
+            # This must run at least once (i.e. do-while semantics).
+            try:
+                message = self._queue.popleft()
+            except IndexError:
+                pass
+            else:
+                messages.append(message)
+
+            if len(messages) == self._current_batch_size_maximum_count:
+                break
+            if not self._countdown_timer.has_time_remaining:
+                break
+
+        self._countdown_timer.reset()
+
+        if len(messages) < self._current_batch_size_minimum_count:
+            # Return the messages to the queue.
+            # Use extendleft alongside reversed to maintain the order.
+            self._queue.extendleft(reversed(messages))
+        else:
+            # Add the marshalled messages to the buffer.
+            marshalled = (self._message_factory.create(message.body)
+                          for message
+                          in messages)
+            self._buffer.extend(marshalled)
+
+    def minimize_batch_size_count(self):
+        self._current_batch_size_maximum_count = self._original_batch_size_minimum_count
+
+    def restore_batch_size_count(self):
+        self._current_batch_size_maximum_count = self._original_batch_size_maximum_count
+
+    def __repr__(self):
+        repr_ = ('{}('
+                 'batch_size_maximum_count={}, '
+                 'batch_size_minimum_count={}, '
+                 'countdown_timer={}, '
+                 'message_factory={})')
+        return repr_.format(self.__class__.__name__,
+                            self._original_batch_size_maximum_count,
+                            self._original_batch_size_minimum_count,
+                            self._countdown_timer,
+                            self._message_factory)
+
+
 class Sqs(consumer.receivers.Buffering):
 
     def __init__(self,
--- clare/clare/infrastructure/tests/test_receivers.py
+++ clare/clare/infrastructure/tests/test_receivers.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import collections
+import itertools
 
 import mock
-from nose.tools import assert_equal, raises
+from nose.tools import assert_equal, assert_raises, assert_true, raises
 
 from .. import receivers
 from clare.common.messaging import consumer
@@ -14,6 +15,225 @@ class MockSqsQueue(object):
 
     def receive_messages(self):
         pass
+
+
+class TestQueue(object):
+
+    def __init__(self):
+        self.message_factory = None
+        self._queue = None
+        self._buffer = None
+
+        self.messages = None
+        self.message = None
+
+    def setup(self):
+        self.message_factory = factories.Message()
+        self._queue = collections.deque()
+        self._buffer = collections.deque()
+
+        self.messages = list()
+        self.messages.append(factories.Message().create(body='foo'))
+        self.messages.append(factories.Message().create(body='bar'))
+        self.messages.append(factories.Message().create(body='foobar'))
+
+        self.message = self.messages[0]
+
+    def test_receive_does_fill_when_buffer_is_empty(self):
+        batch_size_maximum_count = 1
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=itertools.repeat(True))
+
+        self._queue.extend(self.messages)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            countdown_timer=countdown_timer,
+            message_factory=self.message_factory,
+            _queue=self._queue,
+            _buffer=self._buffer)
+
+        expected_queue_count = len(self._queue) - batch_size_maximum_count
+
+        message = receiver.receive()
+
+        assert_equal(expected_queue_count, len(self._queue))
+        assert_equal(batch_size_maximum_count - 1, len(self._buffer))
+        assert_equal(self.message.body, message.body)
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_receive_does_not_fill_while_buffer_has_messages(self):
+        self._queue.extend(self.messages)
+        self._buffer.append(self.message)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=None,
+            countdown_timer=None,
+            message_factory=None,
+            _queue=self._queue,
+            _buffer=self._buffer)
+
+        expected_buffer_count = len(self._buffer) - 1
+
+        message = receiver.receive()
+
+        assert_equal(len(self.messages), len(self._queue))
+        assert_equal(expected_buffer_count, len(self._buffer))
+        assert_equal(self.message.body, message.body)
+
+    def test_receive_timeout_raises_exception(self):
+        batch_size_maximum_count = 1
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=(False,))
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            countdown_timer=countdown_timer,
+            message_factory=None)
+
+        assert_raises(consumer.exceptions.ReceiveTimeout, receiver.receive)
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_receive_is_ordered(self):
+        batch_size_maximum_count = len(self.messages)
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=itertools.repeat(True))
+
+        self._queue.extend(self.messages)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            countdown_timer=countdown_timer,
+            message_factory=self.message_factory,
+            _queue=self._queue)
+
+        for expected_message in self.messages:
+            message = receiver.receive()
+            assert_equal(expected_message.body, message.body)
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_receive_less_than_batch_size_minimum_returns_to_queue(self):
+        batch_size_maximum_count = batch_size_minimum_count = 2
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=(False,))
+
+        self._queue.append(self.message)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            batch_size_minimum_count=batch_size_minimum_count,
+            countdown_timer=countdown_timer,
+            message_factory=None,
+            _queue=self._queue,
+            _buffer=self._buffer)
+
+        expected_queue_count = len(self._queue)
+
+        assert_raises(consumer.exceptions.ReceiveTimeout, receiver.receive)
+        assert_equal(expected_queue_count, len(self._queue))
+        assert_equal(0, len(self._buffer))
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_receive_less_than_batch_size_minimum_returns_to_queue_ordered(self):
+        batch_size_maximum_count = batch_size_minimum_count = 3
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=itertools.chain((True, False), itertools.repeat(True)))
+
+        self._queue.extend(self.messages)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            batch_size_minimum_count=batch_size_minimum_count,
+            countdown_timer=countdown_timer,
+            message_factory=self.message_factory,
+            _queue=self._queue)
+
+        try:
+            receiver.receive()
+        except consumer.exceptions.ReceiveTimeout:
+            receiver.minimize_batch_size_count()
+        except Exception:
+            raise
+
+        for expected_message in self.messages:
+            message = receiver.receive()
+            assert_equal(expected_message.body, message.body)
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_minimize_batch_size_count(self):
+        batch_size_maximum_count = 2
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=(False,))
+
+        self._queue.append(self.message)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            countdown_timer=countdown_timer,
+            message_factory=self.message_factory,
+            _queue=self._queue,
+            _buffer=self._buffer)
+
+        expected_queue_count = len(self._queue) - receivers.BATCH_SIZE_MINIMUM_COUNT
+
+        receiver.minimize_batch_size_count()
+        message = receiver.receive()
+
+        assert_equal(expected_queue_count, len(self._queue))
+        assert_equal(receivers.BATCH_SIZE_MINIMUM_COUNT - 1, len(self._buffer))
+        assert_equal(self.message.body, message.body)
+
+        assert_true(countdown_timer.reset.called)
+
+    def test_restore_batch_size_count(self):
+        batch_size_maximum_count = 2
+        countdown_timer = TestQueue.create_mock_countdown_timer(
+            has_time_remaining=itertools.repeat(True))
+
+        self._queue.extend(self.messages)
+
+        receiver = receivers.Queue(
+            batch_size_maximum_count=batch_size_maximum_count,
+            countdown_timer=countdown_timer,
+            message_factory=self.message_factory,
+            _queue=self._queue,
+            _buffer=self._buffer)
+
+        expected_queue_count = len(self._queue) - batch_size_maximum_count
+
+        receiver.minimize_batch_size_count()
+        receiver.restore_batch_size_count()
+        message = receiver.receive()
+
+        assert_equal(expected_queue_count, len(self._queue))
+        assert_equal(batch_size_maximum_count - 1, len(self._buffer))
+        assert_equal(self.message.body, message.body)
+
+        assert_true(countdown_timer.reset.called)
+
+    @staticmethod
+    def create_mock_countdown_timer(has_time_remaining):
+
+        """
+        Parameters
+        ----------
+        has_time_remaining : collections.Iterable
+
+        Returns
+        -------
+        mock.Mock
+        """
+
+        mock_countdown_timer = mock.Mock()
+        type(mock_countdown_timer).has_time_remaining = mock.PropertyMock(
+            side_effect=has_time_remaining)
+        mock_countdown_timer.reset = mock.Mock()
+        return mock_countdown_timer
 
 
 class TestSqs(object):
