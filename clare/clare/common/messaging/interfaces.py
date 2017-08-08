f3f32345cb32eb18a5a0902424bb27a3dd1840dd
ubuntu <dnguyen0304@gmail.com>
2017-08-08 11:32:20 -0400

    Changed receive to raise ReceiveTimeout for Queue.

--- clare/clare/common/messaging/interfaces.py
+++ clare/clare/common/messaging/interfaces.py
@@ -40,6 +40,12 @@ class Queue(producer.interfaces.ISender):
         Returns
         -------
         clare.common.messaging.models.Message
+
+        Raises
+        ------
+        clare.common.messaging.consumer.ReceiveTimeout
+            If there weren't any buffered messages and the operation
+            took too long to receive the configured number of messages.
         """
 
         raise NotImplementedError
