dd194af65f3cdeac153c6c04529a47e11974e090
ubuntu <dnguyen0304@gmail.com>
2017-08-08 11:58:33 -0400

    Added Receiver.

--- clare/clare/common/messaging/consumer/interfaces.py
+++ clare/clare/common/messaging/consumer/interfaces.py
@@ -74,3 +74,25 @@ class IHandler(object):
         """
 
         raise NotImplementedError
+
+
+class Receiver(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def receive(self):
+
+        """
+        Returns
+        -------
+        clare.common.messaging.models.Message
+
+        Raises
+        ------
+        clare.common.messaging.consumer.ReceiveTimeout
+            If there weren't any buffered messages and the operation
+            took too long to receive the configured number of messages.
+        """
+
+        raise NotImplementedError
--- clare/clare/common/messaging/interfaces.py
+++ clare/clare/common/messaging/interfaces.py
@@ -29,23 +29,6 @@ class IFilter(object):
         pass
 
 
-class Queue(producer.interfaces.ISender):
+class Queue(producer.interfaces.ISender, consumer.interfaces.Receiver):
 
     __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def receive(self):
-
-        """
-        Returns
-        -------
-        clare.common.messaging.models.Message
-
-        Raises
-        ------
-        clare.common.messaging.consumer.ReceiveTimeout
-            If there weren't any buffered messages and the operation
-            took too long to receive the configured number of messages.
-        """
-
-        raise NotImplementedError
