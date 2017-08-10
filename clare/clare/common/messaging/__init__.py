86b55e3ca891955bb563f73a818c4af03d9a55a4
ubuntu <dnguyen0304@gmail.com>
2017-08-09 21:06:24 -0400

    Added Message2.

--- clare/clare/common/messaging/__init__.py
+++ clare/clare/common/messaging/__init__.py
@@ -4,10 +4,12 @@ from . import consumer
 from . import exceptions
 from . import factories
 from . import interfaces
+from . import models
 from . import producer
 
 __all__ = ['consumer',
            'exceptions',
            'factories',
            'interfaces',
+           'models',
            'producer']
--- clare/clare/common/messaging/models.py
+++ clare/clare/common/messaging/models.py
@@ -16,3 +16,31 @@ class Message(object):
     def __repr__(self):
         repr_ = '{}(body="{}")'
         return repr_.format(self.__class__.__name__, self.body)
+
+
+class Message2(object):
+
+    def __init__(self, id, body, delivery_receipt):
+
+        """
+        Parameters
+        ----------
+        id : str
+            Unique identifier.
+        body : str
+            Content.
+        delivery_receipt : str
+            Unique identifier associated with the transaction of
+            receiving this message.
+        """
+
+        self.id = id
+        self.body = body
+        self.delivery_receipt = delivery_receipt
+
+    def __repr__(self):
+        repr_ = '{}(id="{}", body="{}", delivery_receipt="{}")'
+        return repr_.format(self.__class__.__name__,
+                            self.id,
+                            self.body,
+                            self.delivery_receipt)
