399bc1fd79a7d2af620b21cb3fa89ff3b9ca5b30
ubuntu <dnguyen0304@gmail.com>
2017-08-30 11:36:40 -0400

    ME-720: Added InitializationFailed and ExtractFailed.

---
+++ room_list_watcher/exceptions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .common import automation
+
+
+class InitializationFailed(automation.exceptions.ScrapeFailed):
+    pass
+
+
+class ExtractFailed(automation.exceptions.ScrapeFailed):
+    pass
