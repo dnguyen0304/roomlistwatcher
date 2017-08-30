6aea5c7d7d08ad8a34a56c4481a7b36ff01891b7
ubuntu <dnguyen0304@gmail.com>
2017-08-30 12:36:41 -0400

    ME-720: Migrated logging.

--- clare/clare/common/__init__.py
+++ clare/clare/common/__init__.py
@@ -2,12 +2,10 @@
 
 from . import automation
 from . import enumerations
-from . import logging
 from . import messaging
 from . import utilities
 
 __all__ = ['automation',
            'enumerations',
-           'logging',
            'messaging',
            'utilities']
