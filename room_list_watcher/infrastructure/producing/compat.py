61a0cf03349b2e4d9a557395fd149dcb08a182fb
ubuntu <dnguyen0304@gmail.com>
2017-08-30 18:44:37 -0400

    ME-720: Added compat to producing.

---
+++ room_list_watcher/infrastructure/producing/compat.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+_MAJOR_AND_MINOR = slice(0, 2)
+_PYTHON_VERSION = sys.version_info[_MAJOR_AND_MINOR]
+_PYTHON_VERSION_27 = (2, 7)
+
+if _PYTHON_VERSION == _PYTHON_VERSION_27:
+    import httplib as HttpStatus
+else:
+    import http.HTTPStatus as HttpStatus
