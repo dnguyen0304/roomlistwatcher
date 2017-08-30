d4645041982188addb68db4b45cc587727d8a004
ubuntu <dnguyen0304@gmail.com>
2017-08-30 12:33:47 -0400

    ME-720: Migrated io.

--- clare/clare/common/__init__.py
+++ clare/clare/common/__init__.py
@@ -2,14 +2,12 @@
 
 from . import automation
 from . import enumerations
-from . import io
 from . import logging
 from . import messaging
 from . import utilities
 
 __all__ = ['automation',
            'enumerations',
-           'io',
            'logging',
            'messaging',
            'utilities']
--- room_list_watcher/common/__init__.py
+++ room_list_watcher/common/__init__.py
@@ -2,10 +2,12 @@
 
 from . import automation
 from . import event
+from . import io
 from . import retry
 from . import utility
 
 __all__ = ['automation',
            'event',
+           'io',
            'retry',
            'utility']
