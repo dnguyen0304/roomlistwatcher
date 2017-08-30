0b29b1349525ba907d3cc5b7dcd4912d621a47f9
ubuntu <dnguyen0304@gmail.com>
2017-08-30 14:20:06 -0400

    ME-720: Removed IFilter.

--- room_list_watcher/common/messaging/__init__.py
+++ room_list_watcher/common/messaging/__init__.py
@@ -3,11 +3,9 @@
 from . import exceptions
 from . import factories
 from . import filters
-from . import interfaces
 from . import producer
 
 __all__ = ['exceptions',
            'factories',
            'filters',
-           'interfaces',
            'producer']
