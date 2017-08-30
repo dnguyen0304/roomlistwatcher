3bd298d6d6458c142ae75d556f1b99a6b1103272
ubuntu <dnguyen0304@gmail.com>
2017-08-30 13:36:03 -0400

    ME-720: Migrated room_list_watcher.automation.

--- tmp/automation/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import factories
-from . import scrapers
-
-__all__ = ['factories',
-           'scrapers']
--- tmp/automation/__main__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from clare.application.room_list_watcher.automation import factories
-from clare.main import get_configuration
-
-if __name__ == '__main__':
-    properties = get_configuration()['room_list_watcher']['scraper']
-    scraper = factories.Scraper(properties=properties).create()
-    scraper.scrape(url=properties['url'])
