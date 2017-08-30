907e9d6ce1ad85b2092fef18c0ef918f0c77e9d5
ubuntu <dnguyen0304@gmail.com>
2017-08-30 13:42:16 -0400

    ME-720: Changed code sort.

--- room_list_watcher/common/utility.py
+++ room_list_watcher/common/utility.py
@@ -8,6 +8,45 @@ import os
 _ENVIRONMENT_VARIABLE_NAME = 'ROOM_LIST_WATCHER_CONFIGURATION_FILE_PATH'
 
 
+# This implementation closely mirrors the UTC class in pytz and
+# subsequently also the one in the Python Standard Library
+# documentation.
+class UTC(datetime.tzinfo):
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(0)
+    def tzname(self, dt):
+        return 'UTC'
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+    def __repr__(self):
+        repr_ = '{}()'
+        return repr_.format(self.__class__.__name__)
+
+    def __str__(self):
+        return 'UTC'
+
+
+class TimeZone(object):
+
+    @classmethod
+    def from_name(cls, name):
+
+        """
+        Parameters
+        ----------
+        name : str
+
+        Returns
+        -------
+        datetime.tzinfo
+        """
+
+        return UTC()
+
+
 def get_configuration():
 
     """
@@ -39,43 +78,3 @@ def format_exception(e):
     data['exception_message'] = e.message
 
     return json.dumps(data)
-
-
-# This implementation closely mirrors the UTC class in pytz and
-# subsequently also the one in the Python Standard Library
-# documentation.
-class UTC(datetime.tzinfo):
-
-    def utcoffset(self, dt):
-        return datetime.timedelta(0)
-
-    def tzname(self, dt):
-        return 'UTC'
-
-    def dst(self, dt):
-        return datetime.timedelta(0)
-
-    def __repr__(self):
-        repr_ = '{}()'
-        return repr_.format(self.__class__.__name__)
-
-    def __str__(self):
-        return 'UTC'
-
-
-class TimeZone(object):
-
-    @classmethod
-    def from_name(cls, name):
-
-        """
-        Parameters
-        ----------
-        name : str
-
-        Returns
-        -------
-        datetime.tzinfo
-        """
-
-        return UTC()
