1f5ff59540001545569b02cb9bf1470d15c7d075
ubuntu <dnguyen0304@gmail.com>
2017-08-30 14:26:37 -0400

    ME-720: Removed Message.

--- room_list_watcher/common/messaging/__init__.py
+++ room_list_watcher/common/messaging/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import exceptions
-from . import factories
 from . import filters
 from . import producer
 
 __all__ = ['exceptions',
-           'factories',
            'filters',
            'producer']
