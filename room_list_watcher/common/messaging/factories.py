5517211d5a24969536cd1a5b5cbcb3179b72ef56
ubuntu <dnguyen0304@gmail.com>
2017-08-30 14:21:02 -0400

    ME-720: Removed Message.

--- room_list_watcher/common/messaging/factories.py
+++ room_list_watcher/common/messaging/factories.py
@@ -1,39 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from clare.clare.common.messaging import models
-
-
-class Message(object):
-
-    def __init__(self, message_type=models.Message):
-
-        """
-        Parameters
-        ----------
-        message_type : typing.Type[clare.common.messaging.models.Message]
-            Defaults to typing.Type[clare.common.messaging.models.Message].
-        """
-
-        self._message_type = message_type
-
-    def create(self, body):
-
-        """
-        Parameters
-        ----------
-        body : str
-
-        Returns
-        -------
-        clare.common.messaging.models.Message
-        """
-
-        message = self._message_type(body=body)
-        return message
-
-    def __repr__(self):
-        repr_ = '{}(message_type={})'
-        return repr_.format(self.__class__.__name__, self._message_type)
+from . import models
 
 
 class Message2(object):
--- room_list_watcher/common/messaging/models.py
+++ room_list_watcher/common/messaging/models.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-class Message(object):
-
-    def __init__(self, body):
-
-        """
-        Parameters
-        ----------
-        body : str
-        """
-
-        self.body = body
-
-    def __repr__(self):
-        repr_ = '{}(body="{}")'
-        return repr_.format(self.__class__.__name__, self.body)
-
-
 class Message2(object):
 
     def __init__(self, id='', body='', delivery_receipt=''):
