7c64047d979b6d43a298016727fbf75d4440494e
ubuntu <dnguyen0304@gmail.com>
2017-08-30 12:44:19 -0400

    ME-720: Moved Event to events.

--- room_list_watcher/common/event/events.py
+++ room_list_watcher/common/event/events.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import abc
+import collections
+import datetime
+import json
+import uuid
+
+import room_list_watcher.common
+from room_list_watcher.common.logging import interfaces
 
 
 class IEvent(object):
@@ -10,3 +17,43 @@ class IEvent(object):
     @abc.abstractproperty
     def INTERFACE_VERSION(self):
         pass
+
+
+class Event(interfaces.IEvent, room_list_watcher.common.io.JsonSerializable):
+
+    def __init__(self, topic, arguments):
+
+        """
+        Parameters
+        ----------
+        topic : Topic
+        arguments : collections.Mapping
+        """
+
+        self.correlation_id = str(uuid.uuid4())
+
+        self.topic = topic
+        self.arguments = arguments
+
+        time_zone = clare.common.utilities.TimeZone.from_name('UTC')
+        self.created_at = datetime.datetime.utcnow().replace(tzinfo=time_zone)
+
+    @property
+    def INTERFACE_VERSION(self):
+        return '1.0.0'
+
+    def to_json(self):
+        data = collections.OrderedDict()
+        data['interface_version'] = self.INTERFACE_VERSION
+        data['correlation_id'] = self.correlation_id
+        data['topic_name'] = self.topic.name
+        data['arguments'] = self.arguments
+        data['created_at'] = self.created_at.isoformat()
+
+        return json.dumps(data)
+
+    def __repr__(self):
+        repr_ = '{}(topic={}, arguments={})'
+        return repr_.format(self.__class__.__name__,
+                            self.topic,
+                            self.arguments)
--- room_list_watcher/common/logging/__init__.py
+++ room_list_watcher/common/logging/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import interfaces
-from .event import Event
 from . import utilities
 
-__all__ = ['Event',
-           'interfaces',
-           'utilities']
+__all__ = ['utilities']
