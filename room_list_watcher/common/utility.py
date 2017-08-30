a5ae5a38c3ec943bbc1de1d55e31dfc2fcb6273e
ubuntu <dnguyen0304@gmail.com>
2017-08-30 13:09:21 -0400

    ME-720: Migrated UTC to utility.

--- room_list_watcher/common/utility.py
+++ room_list_watcher/common/utility.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import collections
+import datetime
 import json
 import os
 
@@ -38,3 +39,43 @@ def format_exception(e):
     data['exception_message'] = e.message
 
     return json.dumps(data)
+
+
+# This implementation closely mirrors the UTC class in pytz and
+# subsequently also the one in the Python Standard Library
+# documentation.
+class UTC(datetime.tzinfo):
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(0)
+
+    def tzname(self, dt):
+        return 'UTC'
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+    def __repr__(self):
+        repr_ = '{}()'
+        return repr_.format(self.__class__.__name__)
+
+    def __str__(self):
+        return 'UTC'
+
+
+class TimeZone(object):
+
+    @classmethod
+    def from_name(cls, name):
+
+        """
+        Parameters
+        ----------
+        name : str
+
+        Returns
+        -------
+        datetime.tzinfo
+        """
+
+        return UTC()
