da5c8df357cd3bccfb33551030c8197cff84d6d0
ubuntu <dnguyen0304@gmail.com>
2017-08-30 20:27:33 -0400

    ME-720: Migrated __main__.

--- room_list_watcher/__main__.py
+++ room_list_watcher/__main__.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from room_list_watcher import factories
+from room_list_watcher import infrastructure
 from room_list_watcher.common import utility
 
 if __name__ == '__main__':
-    properties = utility.get_configuration()['scraper']
-    scraper = factories._Scraper(properties=properties).create()
-    scraper.scrape(url=properties['url'])
-    scraper.dispose()
+    properties = utility.get_configuration()
+
+    infrastructure_factory = infrastructure.factories.RoomListWatcherInfrastructure(
+        properties=properties)
+    infrastructure = infrastructure_factory.create()
+
+    application_factory = factories.RoomListWatcher(
+        infrastructure=infrastructure,
+        properties=properties)
+    application = application_factory.create()
+
+    application.start()
