060a712e719012a010d070f90ea739b64c50d4eb
ubuntu <dnguyen0304@gmail.com>
2017-09-01 10:41:35 -0400

    ME-720: Changed IncrementingFilePath name to "TimestampingFilePath".

--- room_list_watcher/infrastructure/producing/tests/test_generators.py
+++ room_list_watcher/infrastructure/producing/tests/test_generators.py
@@ -10,7 +10,7 @@ from .. import generators
 class TestTimestampingFilePath(object):
 
     def setup(self):
-        self.template = '[\w/]+{qualifier_delimiter}(?P<counter>\d)\.\w+'
+        self.template = '[\w/]+{qualifier_delimiter}(?P<>\d)\.\w+'
 
         self.directory_path = '/home/foo/'
         self.file_name = 'bar'
@@ -33,19 +33,19 @@ class TestTimestampingFilePath(object):
         file_path = generator.generate()
         assert_is_not_none(re.match(pattern, file_path))
 
-    def counter_is_appended(self):
+    def _is_appended(self):
         pattern = self.template.format(
             qualifier_delimiter=self.qualifier_delimiter)
         file_path = self.generator.generate()
         assert_is_not_none(re.match(pattern, file_path))
 
-    def counter_is_incremented(self):
+    def _is_incremented(self):
         pattern = self.template.format(
             qualifier_delimiter=self.qualifier_delimiter)
         match = re.match(pattern=pattern, string=self.generator.generate())
-        before = int(match.groupdict()['counter'])
+        before = int(match.groupdict()[''])
         match = re.match(pattern=pattern, string=self.generator.generate())
-        after = int(match.groupdict()['counter'])
+        after = int(match.groupdict()[''])
         assert_equal(before, after - 1)
 
     def test_from_file_path(self):
