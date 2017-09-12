15f2b458128b5556b304a10b689d3c5a08863c8c
ubuntu <dnguyen0304@gmail.com>
2017-09-12 00:52:11 -0400

    ME-720: Added tag local variable.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+DOMAIN="dnguyen0304"
 NAMESPACE="roomlistwatcher"
 VERSION=$(grep -Po "version='\K\d\.\d\.\d" setup.py)
 REMOTE_SHARED_VOLUME="/tmp/build"
@@ -9,22 +10,27 @@ REMOTE_SHARED_VOLUME="/tmp/build"
 rm --force ${NAMESPACE}*.zip
 
 # Create the buildtime container.
+tag=${DOMAIN}/${NAMESPACE}-buildtime:${VERSION}
+
 docker build \
     --file docker/buildtime/Dockerfile \
-    --tag dnguyen0304/${NAMESPACE}-buildtime:${VERSION} \
+    --tag ${tag} \
     --build-arg COMPONENT=${NAMESPACE} \
     --build-arg SHARED_VOLUME=${REMOTE_SHARED_VOLUME} \
     .
+
 # Create the package.
 docker run \
     --volume $(pwd):${REMOTE_SHARED_VOLUME} \
-    ${NAMESPACE}-buildtime:${VERSION} \
+    ${tag} \
     ${NAMESPACE} ${REMOTE_SHARED_VOLUME} ${VERSION}
 
 # Create the runtime container.
+tag=${DOMAIN}/${NAMESPACE}:${VERSION}
+
 docker build \
     --file docker/runtime/Dockerfile \
-    --tag dnguyen0304/${NAMESPACE}:${VERSION} \
+    --tag ${tag} \
     --build-arg NAMESPACE=${NAMESPACE} \
     --build-arg CONFIGURATION_FILE_NAME="application.config" \
     --build-arg AWS_CONFIGURATION_FILE_NAME="aws.config" \
